bba6b9ca0409fae468353b41fd3d93a4b72053ca
Alfred-Onuada <aonuada5@gmail.com>
2023-06-04 00:06:01 +0100

    removed route for transaction history ahead of submission

--- routes/wallet.routes.ts
+++ routes/wallet.routes.ts
@@ -7,8 +7,6 @@ const router = Router();
 // requires authentication on all routes within this router
 router.use(isLoggedIn);
 
-router.get('/history');
-
 router.post('/verify/:ref', verifyTopup);
 
 router.post('/transfer', transferFunds);
