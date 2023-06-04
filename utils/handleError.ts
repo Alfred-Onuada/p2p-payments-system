0402b10a19abfc1593700feae6d2e95fc1bf6806
Alfred-Onuada <aonuada5@gmail.com>
2023-06-04 08:17:14 +0100

    refactored handle error function

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -2,61 +2,87 @@ import { Response } from "express";
 import { IMongoError } from "../interfaces/mongoerror";
 
 export const handleAPIError = function (error: IMongoError, res: Response) {
-  // process error from mongodb schema validation
-  if (error.name === "ValidationError") {
-    // get all validation errors
-    const validationErrors = Object.values(error.errors).map((err) => {
-      // identify and parse cast errors
-      if (err.name === "CastError") {
-        return `The value ${err.stringValue.replace(
-          /\"/g,
-          "'"
-        )} doesn't match the required type for that field`;
-      }
-
-      return err.message;
-    });
-
-    // send error to client
-    res.status(400).json({
-      message: "Bad Request",
-      errors: validationErrors,
-    });
+  if (isValidationError(error)) {
+    const validationErrors = getValidationErrors(error);
+    sendValidationErrorsResponse(res, validationErrors);
     return;
   }
 
-  // process error from mongodb duplicate key
-  if (error.code === 11000) {
-    let duplicateField = Object.keys(error.keyPattern)[0];
-
-    // when a user tries to verify a transaction multiple times
-    if (duplicateField === "ref") {
-      res
-        .status(400)
-        .json({ message: `Transaction has already been verified` });
-      return;
-    }
-
-    res.status(400).json({ message: `${duplicateField} already exists` });
+  if (isDuplicateKeyError(error)) {
+    sendDuplicateKeyErrorResponse(res, error);
     return;
   }
 
-  // this particular error won't happen in production as mongoDB will be deployed using a replica set
-  if (
-    error.toString() ===
-    "MongoServerError: Transaction numbers are only allowed on a replica set member or mongos"
-  ) {
-    console.log(
-      "\x1b[31m%s\x1b[0m",
-      "Attention Please!!! the last request failed because you are running this application with a standalone mongoDB deployment, please switch to a replica set"
-    );
-  }
-
-  // process errors from interacting with paystack
-  if (error.name === "PaystackError") {
-    res.status(400).json({ message: error.message });
+  if (isMongoDBDeploymentError(error)) {
+    handleMongoDBDeploymentError();
     return;
   }
 
-  res.status(500).json({ message: "Internal Server Error" });
+  if (isPaystackError(error)) {
+    sendPaystackErrorResponse(res, error);
+    return;
+  }
+
+  sendInternalErrorResponse(res);
 };
+
+function isValidationError(error: IMongoError): boolean {
+  return error.name === "ValidationError";
+}
+
+function getValidationErrors(error: IMongoError): string[] {
+  return Object.values(error.errors).map((err) => {
+    if (err.name === "CastError") {
+      return parseCastErrorMessage(err.stringValue.toString());
+    }
+    return err.message.toString();
+  });
+}
+
+function parseCastErrorMessage(value: string): string {
+  return `The value ${value.replace(/\"/g, "'")} doesn't match the required type for that field`;
+}
+
+function sendValidationErrorsResponse(res: Response, errors: string[]): void {
+  res.status(400).json({
+    message: "Bad Request",
+    errors,
+  });
+}
+
+function isDuplicateKeyError(error: IMongoError): boolean {
+  return error.code === 11000;
+}
+
+function sendDuplicateKeyErrorResponse(res: Response, error: IMongoError): void {
+  const duplicateField = Object.keys(error.keyPattern)[0];
+
+  if (duplicateField === "ref") {
+    res.status(400).json({ message: `Transaction has already been verified` });
+  } else {
+    res.status(400).json({ message: `${duplicateField} already exists` });
+  }
+}
+
+function isMongoDBDeploymentError(error: IMongoError): boolean {
+  return error.toString() === "MongoServerError: Transaction numbers are only allowed on a replica set member or mongos";
+}
+
+function handleMongoDBDeploymentError(): void {
+  console.log(
+    "\x1b[31m%s\x1b[0m",
+    "Attention Please!!! the last request failed because you are running this application with a standalone mongoDB deployment, please switch to a replica set"
+  );
+}
+
+function isPaystackError(error: IMongoError): boolean {
+  return error.name === "PaystackError";
+}
+
+function sendPaystackErrorResponse(res: Response, error: IMongoError): void {
+  res.status(400).json({ message: error.message });
+}
+
+function sendInternalErrorResponse(res: Response): void {
+  res.status(500).json({ message: "Internal Server Error" });
+}
